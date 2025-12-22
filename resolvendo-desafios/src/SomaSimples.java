003da2a4aab0f2870d0cb95573acc7256c9b819a
Vinícius Araújo Coelho <viniciusaccoelho123@gmail.com>
2025-12-22 20:08:30 -0300

    resolvendo desafios - fundamentos da linguagem de programação java

---
+++ resolvendo-desafios/src/SomaSimples.java
@@ -0,0 +1,45 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.util.Scanner;
+
+public class SomaSimples {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int A, B, soma;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        //TODO: Complete os espaços em branco com uma possível solução para o desafio
+        soma = A + B;
+
+        System.out.println("SOMA = " + soma);
+    }
+}
+
+/*
+    Desafio
+    Leia dois valores inteiros identificados como variáveis A
+    e B. Calcule a soma entre elas e chame essa variável de
+    SOMA.
+    A seguir escreva o valor desta variável.
+
+    Entrada
+    O arquivo de entrada contém 2 valores inteiros.
+
+    Saída
+    Imprima a variável SOMA com todas as letras
+    maiúsculas, inserindo um espaço em branco antes e
+    depois do símbolo de igualdade, seguido pelo valor
+    correspondente à soma de A e B.
+
+    Exemplos de Entrada        Exemplos de Saída
+    30                         SOMA = 40
+    10
+
+    -30                        SOMA = -20
+    10
+
+    0                          SOMA = 0
+    0
+*/
